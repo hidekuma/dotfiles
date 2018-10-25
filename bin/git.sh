462267405c6e946532e2f30756181a730fbbc74d
hidekuma <d.hidekuma@gmail.com>
2018-10-26 01:09:05 +0900

    update

--- bin/git.sh
+++ bin/git.sh
@@ -4,3 +4,4 @@ git config --global user.name "hidekuma"
 git config --global user.email "d.hidekuma@gmail.com"
 git config --global core.precomposeunicode true
 git config --global core.quotepath false
+git config --global credential.helper 'cache --timeout=432000'
