5c11b5a77c2bdc85aab678a8a4e4efaeb82b4857
hidekuma <d.hidekuma@gmail.com>
2018-11-07 23:13:57 +0900

    mysql8to5.7

--- bin/mysql8to5.7.sh
+++ bin/mysql8to5.7.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo 'export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"' >> ~/.zsh_profile
+
+
+# if update MacOS to mojave then apple install mysql8. mysql8 have some problems... 
+# so.. i wanna use mysql@5.7
+# 1) brew install mysql@5.7
+# 2) brew install mysql-connector-c
+# 3) pip install msysqlcient
