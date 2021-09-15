95a8444ccd0582f46de4605292bf65cda41aec4f
hidekuma <d.hidekuma@gmail.com>
2021-09-15 11:37:53 +0900

    updated

--- bin/install-virtualenv.sh
+++ bin/install-virtualenv.sh
@@ -3,5 +3,6 @@ brew install python3
 pip3 install virtualenv virtualenvwrapper
 mkdir ~/.virtualenvs
 echo "export WORKON_HOME=~/.virtualenvs" >> ../.zsh_profile
-echo "export VIRTUALENVWRAPPER_PYTHON='$(which python3)'" >> ../.zsh_profile
-echo "source $(find /usr -name virtualenvwrapper.sh)" >> ../.zsh_profile 
+echo "export VIRTUALENVWRAPPER_PYTHON=$(which python3)" >> ../.zsh_profile
+echo "source $(find /.local -name virtualenvwrapper.sh)" >> ../.zsh_profile 
+#echo "source $(find /usr -name virtualenvwrapper.sh)" >> ../.zsh_profile 
