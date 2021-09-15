727efc54fe20102467affa567d3a7fb5fa5617bc
hidekuma <d.hidekuma@gmail.com>
2021-09-15 11:09:12 +0900

    updated

--- bin/install-nvim.sh
+++ bin/install-nvim.sh
@@ -3,3 +3,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 ./squashfs-root/usr/bin/nvim
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
