7d5350ac5e6a1717d48a376a3b248e1ba7b75ecc
hidekuma <d.hidekuma@gmail.com>
2022-07-19 01:08:00 +0900

    fix: add kt file type

--- nvim/lua/user/lsp/settings/jdtls.lua
+++ nvim/lua/user/lsp/settings/jdtls.lua
@@ -1,14 +1,3 @@
-local lspconfig = require("lspconfig")
-
 return {
-    use_lombok_agent = true,
-    vmargs = {
-        "-XX:+UseParallelGC",
-        "-XX:GCTimeRatio=4",
-        "-XX:AdaptiveSizePolicyWeight=90",
-        "-Dsun.zip.disableMemoryMapping=true",
-        "-Djava.import.generatesMetadataFilesAtProjectRoot=false",
-        "-Xmx1G",
-        "-Xms100m",
-    }
+    filetypes = { "java" }
 }
---
+++ nvim/lua/user/lsp/settings/kotlin_language_server.lua
@@ -0,0 +1,4 @@
+return {
+    root_dir = root_pattern("settings.gradle", "settings.gradle.kts", "build.gradle", "build.gradle.kts"),
+    filetypes = { "kotlin", "kt" }
+}
