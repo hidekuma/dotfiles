d06a3d8ecfd9d99bc57e5383f9f705e54ed20ac9
hidekuma <d.hidekuma@gmail.com>
2022-08-05 22:15:05 +0900

    fix: update whichkey

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -90,7 +90,7 @@ local mappings = {
   ["q"] = { "<cmd>q!<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
-  ["f"] = {
+  ["ff"] = {
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Find files",
   },
