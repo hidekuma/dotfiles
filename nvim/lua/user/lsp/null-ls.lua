3cec4e7c3e9a7654e1d407bc8bfb8fbd952e4bb6
hidekuma <d.hidekuma@gmail.com>
2022-10-12 21:28:08 +0900

    feat: update null-ls.lua

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -24,9 +24,9 @@ null_ls.setup({
 		}),
 		diagnostics.djlint,
 		--[[ diagnostics.eslint_d, ]]
-		code_actions.gitsigns,
+		--[[ code_actions.gitsigns, ]]
 		--[[ formatting.isort, ]]
-		-- diagnostics.mypy,
+		diagnostics.mypy,
 	},
 	root_dir = lspconfig.util.root_pattern(".null-ls-root", "Makefile", ".git", "pyproject.toml"),
 	-- you can reuse a shared lspconfig on_attach callback here
