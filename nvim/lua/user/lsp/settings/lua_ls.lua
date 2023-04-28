2b0cee4e47bbba5c16d2a8ceec3018bad4a54202
hidekuma <d.hidekuma@gmail.com>
2023-04-28 11:45:44 +0900

    fix: update lua_ls

---
+++ nvim/lua/user/lsp/settings/lua_ls.lua
@@ -0,0 +1,24 @@
+return {
+	settings = {
+		Lua = {
+			runtime = {
+				--[[ version = "LuaJit", ]]
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+}
