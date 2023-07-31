efe96f3513af6c4b74835ee0c6e894ef71a975ba
hidekuma <d.hidekuma@gmail.com>
2023-07-31 10:47:48 +0900

    fix: update command for copilot

--- nvim/lua/user/copilot.lua
+++ nvim/lua/user/copilot.lua
@@ -1,5 +1,5 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<C-K>", "copilot#Next()", { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-L>", "copilot#Next()", { silent = true, expr = true })
