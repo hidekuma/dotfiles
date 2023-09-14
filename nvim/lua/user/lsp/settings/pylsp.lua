76461035c63b15b98aa4c05e04d73761a3edf4d3
hidekuma <d.hidekuma@gmail.com>
2023-09-14 17:20:28 +0900

    fix(pylsp): update lsp black

--- nvim/lua/user/lsp/settings/pylsp.lua
+++ nvim/lua/user/lsp/settings/pylsp.lua
@@ -3,15 +3,18 @@ local venv_path = os.getenv('VIRTUAL_ENV')
 local py_path = nil
 -- decide which python executable to use for mypy
 if venv_path ~= nil then
-py_path = venv_path .. "/bin/python3"
+    py_path = venv_path .. "/bin/python3"
 else
-py_path = vim.g.python3_host_prog
+    py_path = vim.g.python3_host_prog
 end
 
 
 -- NOTE
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pylsp
 -- ~/.local/share/nvim/mason/bin/pylsp 이쪽에 들어가서 서드파티black 을 pip install 해줘야 black formatting 사용가능
+-- 2023/09/14 updated 경로 변경됨, 재설치
+-- pip install python-lsp-black
+-- /Users/joseph/.local/share/nvim/mason/packages/python-lsp-server/venv
 return {
     settings = {
         pylsp = {
