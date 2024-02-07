d27fba2f358f930ac0442fe1d09b6e7a85091e6c
hidekuma <d.hidekuma@gmail.com>
2024-02-07 21:40:36 +0900

    feat: add font

--- bin/install-fonts.sh
+++ bin/install-fonts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# https://github.com/ryanoasis/nerd-fonts
-cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+brew tap homebrew/cask-fonts
+brew install font-caskaydia-cove-nerd-font
