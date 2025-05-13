69cb2dfc65acf040466e0e02feccba52957e9e3e
hidekuma <d.hidekuma@gmail.com>
2025-05-14 08:31:06 +0900

    fix: copliot

--- nvim/lua/user/copilot.lua
+++ nvim/lua/user/copilot.lua
@@ -1,6 +1,6 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
-vim.g.copilot_node_command = "~/.nvm/versions/node/v20.15.1/bin/node"
+-- vim.g.copilot_node_command = "~/.nvm/versions/node/v20.15.1/bin/node"
 vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-L>", "copilot#Next()", { silent = true, expr = true })
