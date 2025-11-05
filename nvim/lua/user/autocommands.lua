726659567e9ef590d6798e4d39cbdeb82df90b55
hidekuma <d.hidekuma@gmail.com>
2025-11-05 13:52:36 +0900

    fix: add noformat

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -42,9 +42,58 @@ vim.cmd [[
 --   autocmd BufWritePre * lua vim.lsp.buf.format { async = true }
 -- augroup end
 vim.api.nvim_create_augroup('AutoFormatting', {})
+local function should_format(bufnr)
+  if vim.g.disable_autoformat then
+    return false
+  end
+
+  if vim.b[bufnr].disable_autoformat then
+    return false
+  end
+
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  if bufname == "" then
+    return false
+  end
+
+  local disable_markers = { ".nvim-disable-format", ".disable-format", ".noformat" }
+  local dir = vim.fs.dirname(bufname)
+  local found = vim.fs.find(disable_markers, {
+    path = dir,
+    upward = true,
+    stop = vim.loop.os_homedir(),
+  })
+
+  return vim.tbl_isempty(found)
+end
+
 vim.api.nvim_create_autocmd('BufWritePre', {
   group = 'AutoFormatting',
-  callback = function()
-    vim.lsp.buf.format({ async = false })
+  callback = function(args)
+    if not should_format(args.buf) then
+      return
+    end
+
+    vim.lsp.buf.format({ async = false, bufnr = args.buf })
   end,
 })
+
+vim.api.nvim_create_user_command('AutoFormatToggle', function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.b[bufnr].disable_autoformat = not vim.b[bufnr].disable_autoformat
+  if vim.b[bufnr].disable_autoformat then
+    vim.notify("Autoformat disabled for current buffer", vim.log.levels.INFO)
+  else
+    vim.notify("Autoformat enabled for current buffer", vim.log.levels.INFO)
+  end
+end, { desc = 'Toggle LSP autoformat on save for current buffer' })
+
+vim.api.nvim_create_user_command('AutoFormatDisable', function()
+  vim.g.disable_autoformat = true
+  vim.notify("Autoformat disabled globally", vim.log.levels.INFO)
+end, { desc = 'Disable LSP autoformat on save globally' })
+
+vim.api.nvim_create_user_command('AutoFormatEnable', function()
+  vim.g.disable_autoformat = false
+  vim.notify("Autoformat enabled globally", vim.log.levels.INFO)
+end, { desc = 'Enable LSP autoformat on save globally' })
