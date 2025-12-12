0ef747018d0ad21f6a3fda067f9b850df26539b3
hidekuma <d.hidekuma@gmail.com>
2025-12-12 09:52:52 +0900

    fix

--- nvim/lua/user/compat.lua
+++ nvim/lua/user/compat.lua
@@ -72,30 +72,4 @@ if vim.islist and vim.tbl_islist then
   end
 end
 
--- Provide backwards compatible vim.validate that supports the legacy table form.
-do
-  local validate_new = vim.validate
-  if type(validate_new) == "function" then
-    vim.validate = function(arg1, ...)
-      if type(arg1) == "table" then
-        for name, spec in pairs(arg1) do
-          local value = spec[1]
-          local validator = spec[2]
-          local optional = spec[3]
-          local message = spec[4]
-
-          if type(optional) == "string" and message == nil then
-            message = optional
-            optional = nil
-          end
-
-          validate_new(name, value, validator, optional, message)
-        end
-        return true
-      end
-      return validate_new(arg1, ...)
-    end
-  end
-end
-
 return M
