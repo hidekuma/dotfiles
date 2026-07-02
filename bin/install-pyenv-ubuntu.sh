3673716c2bddc5195effecc507e657851d5c902e
hidekuma <d.hidekuma@gmail.com>
2026-07-02 14:52:16 +0900

    fix

--- bin/install-pyenv-ubuntu.sh
+++ bin/install-pyenv-ubuntu.sh
@@ -2,6 +2,6 @@
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
 # shellcheck source=/dev/null
 source ~/.bashrc
